f410dd2fbc12dd02342569cfc650e21d2582745f
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:34:47 +0100

    first commit

---
+++ immy_numpy_journey_print_datatype.py
@@ -0,0 +1,4 @@
+#TASK: Create a function which receives a Numpy array as parameter and prints it data type on stdout.
+import numpy as np
+def my_numpy_journey_print_datatype(np_array):
+    print(np_array.dtype)
